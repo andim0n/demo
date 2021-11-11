170333521cd97ef7541da002a45e8494ccd9cf32
Andrew Petrov <andypetroff97@gmail.com>
2021-11-11 19:58:47 +0300

    upd

--- cypress/integration/commands.test.js
+++ cypress/integration/commands.test.js
@@ -1,10 +1,15 @@
 context('Cypress custom commands', () => {
   it('commands to work with session storage', () => {
+    // The read-only sessionStorage property accesses a session Storage object for the current origin.
     cy.setSessionStorage('token', 'abc123')
     cy.getSessionStorage('token').should('eq', 'abc123')
   })
 
   it('overwrited visit', () => {
     cy.visit('https://yandex.ru', { search: true, timeout: 10000 })
+  })
+
+  it('overwrited visit', () => {
+    cy.visit('https://yandex.ru', {})
   })
 })
