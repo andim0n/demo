e6816552f37bea2fb6fc4b21db93569c3763fe5a
Andrew Petrov <andypetroff97@gmail.com>
2021-11-18 23:34:17 +0300

    comments and useless hook deleted

--- cypress/integration/demo/1_request.test.js
+++ cypress/integration/demo/1_request.test.js
@@ -1,33 +1,21 @@
-describe('', () => {
-  before(() => {
-    cy.visit('https://example.cypress.io/commands/network-requests')
-  })
+/// <reference types="cypress" />
 
-  it('cy.request() - save response in the shared test context', () => {
-    // https://on.cypress.io/variables-and-aliases
-    cy.request('https://jsonplaceholder.cypress.io/users?_limit=1')
+describe('Request', () => {
+  it('save response in the shared test context', () => {
+    cy.request('https://jsonplaceholder.cypress.io/users')
       .its('body')
-      .its('0') // yields the first element of the returned list
-      .as('user') // saves the object in the test context
+      .its('0')
+      .as('user')
       .then(function () {
-        // NOTE 👀
-        //  By the time this callback runs the "as('user')" command
-        //  has saved the user object in the test context.
-        //  To access the test context we need to use
-        //  the "function () { ... }" callback form,
-        //  otherwise "this" points at a wrong or undefined object!
         cy.request('POST', 'https://jsonplaceholder.cypress.io/posts', {
           userId: this.user.id,
           title: 'Cypress Test Runner',
           body: 'Fast, easy and reliable testing for anything that runs in a browser.',
         })
           .its('body')
-          .as('post') // save the new post from the response
+          .as('post')
       })
       .then(function () {
-        // When this callback runs, both "cy.request" API commands have finished
-        // and the test context has "user" and "post" objects set.
-        // Let's verify them.
         expect(this.post, 'post has the right user id').property('userId').to.equal(this.user.id)
       })
   })
